e1b8aab990467a27e15ea96ce2c025032f6751b7
James van Arkel <j.van.arkel@brickparking.com>
2015-07-22 14:02:09 +0200

    installation for the Grunt features (minify and bundle of both the js and css) therefore there needed to be a little bit of spring cleaning and rearranging.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-wiredep');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('changes', ['watch']);
-
+  'use-strict';
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);  
   grunt.initConfig({
     wiredep: {
       app: {
@@ -12,12 +10,65 @@ module.exports = function (grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-        'js/*.js'
+        'assets/js/*.js'
       ]
     },
     watch: {
-      files: ['bower_components/*'],
-      tasks: ['wiredep']
-    }
+      bower: {
+        files: ['bower_components/*'],
+        tasks: ['wiredep']
+      },
+      cssmin: {
+        files: [
+          'assets/style/css/style.css',
+          'assets/style/css/offline-theme-dark.css'
+        ],
+        task: ['cssmin']
+      },
+      uglify: {
+        files: [
+          'assets/js/app.js',
+          'assets/js/places.js',
+          'assets/js/style.js'
+        ],
+        task: ['uglify']
+      }
+    },
+    cssmin: {  
+      sitecss: {  
+        options: {  
+          banner: '/* My minified css file */'  
+        },  
+        files: {  
+          'public/css/site.min.css': [  
+            'assets/style/css/style.css',
+            'assets/style/css/offline-theme-dark.css'
+          ]  
+        }  
+      }  
+    },
+    uglify: {  
+      options: {  
+        compress: true  
+      },  
+      applib: {  
+        src: [  
+          'assets/js/app.js',
+          'assets/js/places.js',
+          'assets/js/style.js'
+        ],  
+        dest: 'public/js/applib.min.js'  
+      }  
+    }    
   });
+  
+  // Default task.  
+  grunt.registerTask('default', ['uglify', 'cssmin']);  
+  //Load local tasks
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('changes', ['watch']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
 };
---
+++ assets/js/app.js
@@ -0,0 +1,111 @@
+'use strict';
+/**
+ *    @TITLE: OLD SKOOL AMSTERDAM MAP
+ *    @AUTHOR: JAMES VAN ARKEL FOR UDACITY PROJECT 5
+ *    @DATE: JULY 2015
+ */
+var Place = function (data) {
+  var self = this;
+  self.name = data.name;
+  self.position = data.latLng;
+  self.marker = '';
+  self.selected = ko.observable(false);
+
+  self.init = function () {
+    self.createMarker();
+  };
+
+  self.createMarker = function () {
+    self.marker = new google.maps.Marker({
+      map: googleMap,
+      position: self.position,
+      animation: google.maps.Animation.DROP,
+      icon: 'public/img/pin.png'
+    });
+  };
+
+  self.addMarker = function () {
+    self.marker.setMap(googleMap);
+  };
+
+  self.removeMarker = function () {
+    self.marker.setMap(null);
+  };
+  self.select = function () {
+    if (appView !== undefined) {
+      appView.closeInfowindow();
+      appView.selected(false);
+    }
+  };
+  self.init();
+};
+
+
+var ViewModel = function () {
+  var self = this;
+  self.filterList = ko.observableArray();
+  self.filterText = ko.observable();
+  console.log(places());
+
+  // Get the places out of the observable array in places.js and shoot them on the map
+  self.search = function (data) {
+    var filter = data();
+    self.filterList.removeAll();
+    self.removeMarkerAll();
+    
+    if (!filter) { filter = "";}
+    for (var i = 0, len = places().length; i < len; i++) {
+      if(places()[i].name.toLowerCase().indexOf(filter.toLowerCase()) >= 0) {
+        places()[i].addMarker();  
+        self.filterList.push(places()[i]);
+      }
+    }
+  }
+  self.removeMarkerAll = function () {
+      
+    for (var i = 0; i < places().length; i++) {
+      places()[i].removeMarker();
+    }
+  
+	};
+  self.init = function () {
+    MapInit();
+    self.search(self.filterText);
+  };
+};
+  
+// Initialize Google Map
+var googleMap;
+function MapInit() {
+  var mapOptions = {
+    center: { lat: 52.3667, lng: 4.9000 },
+    zoom: 13,
+    panControl: false,
+    scaleControl: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DEFAULT,
+      mapTypeIds: [
+        google.maps.MapTypeId.ROADMAP,
+      ]
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    }
+  };
+  googleMap = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+  // grab the styles out of the style.js to make the map awesome.
+  googleMap.setOptions({ styles: styles });
+};
+
+// 1...2..3..GO!
+var appView;
+document.addEventListener("DOMContentLoaded", function (event) { 
+
+  appView = new ViewModel();
+  ko.applyBindings(appView);
+  appView.init();
+
+});
